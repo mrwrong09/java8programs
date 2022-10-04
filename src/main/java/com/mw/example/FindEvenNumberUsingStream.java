62701f75bd730a9a6f0a1e4b64bfdaf5f83f961e
Prabhat Satpathi <prabhat09satpathi@gmail.com>
2022-10-04 16:53:35 +0530

    initial commit

---
+++ src/main/java/com/mw/example/FindEvenNumberUsingStream.java
@@ -0,0 +1,16 @@
+package com.mw.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindEvenNumberUsingStream {
+    public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .forEach(System.out::println);
+
+    }
+}
